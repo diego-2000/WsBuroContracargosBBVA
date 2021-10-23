0538bed876d56938cd8370880427aeab5fe26014
diego del rio soto <delriosotoluisdiego@gmail.com>
2021-10-23 10:38:25 -0500

    first commit

---
+++ src/main/java/com/web/api/buro/contracargos/WsBuroContracargosBbvaApplication.java
@@ -0,0 +1,13 @@
+package com.web.api.buro.contracargos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsBuroContracargosBbvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsBuroContracargosBbvaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/api/buro/contracargos/WsBuroContracargosBbvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.api.buro.contracargos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsBuroContracargosBbvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
